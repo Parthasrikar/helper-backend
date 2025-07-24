82eb2371f88aa83b1a8c0bb429aab888af8d9d53
parthasrikar <srikar.p@inncircles.com>
2025-07-24 10:24:08 +0530

    set the project with mongodb url with cors enabled and set up the global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin : 'http://localhost:4200/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+ bootstrap();
